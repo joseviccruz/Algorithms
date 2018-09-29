b2a2f62a7da8deb858a35abc54438efe1ac38af6
José Victor Cruz <jvsc@cin.ufpe.br>
2018-09-28 22:33:06 -0300

    Update 380C - Sereja and Brackets.cpp

--- Problems/Segment Tree/Codeforces/380C - Sereja and Brackets.cpp
+++ Problems/Segment Tree/Codeforces/380C - Sereja and Brackets.cpp
@@ -2,42 +2,39 @@
 
 using namespace std;
 
-const int oo = 1e9;
+inline int L(const int &x) { return (x << 1); }
+inline int R(const int &x) { return (x << 1)|1; }
 
 struct node {
-	int l, r;
-	node(const int l = +oo, const int r = -oo) : l(l), r(r) {}
-	inline node operator + (const int x) const { return node(min(l, x), max(r, x)); }
-	inline node operator + (const node x) const { return node(min(l, x.l), max(r, x.r)); }
+	int ans, l, r;
+	node(int _ans = 0, int _l = 0, int _r = 0) : ans(_ans), l(_l), r(_r) {}
+	inline node operator + (const node x) const {
+		int aux = min(l, x.r);
+		return node(ans + x.ans + aux, l + x.l - aux, r + x.r - aux);
+	}
 };
 
 int n, m;
 string s;
-vector<int> op;
-vector<vector<node>> t;
+vector<node> t;
 
-inline int L(const int &x) { return (x << 1); }
-inline int R(const int &x) { return (x << 1)|1; }
-
-// 0 == '(' // 1 == ')'
-
-inline void build(const bool &o) {
+inline void build() {
 	for(int x = n - 1; x; x--) {
-		t[x][o] = t[L(x)][o] + t[R(x)][o];
+		t[x] = t[L(x)] + t[R(x)];
 	}
 }
 
-inline node query(int l, int r, const bool &o) {
-	node ans;
+inline int query(int l, int r) {
+	node ansl, ansr;
 	for(l += n, r += n + 1; l < r; l >>= 1, r >>= 1) {
 		if(l & 1) {
-			ans = t[l++][o] + ans;
+			ansl = ansl + t[l++];
 		}
 		if(r & 1) {
-			ans = ans + t[--r][o];
+			ansr = t[--r] + ansr;
 		}
 	}
-	return ans;
+	return (ansl + ansr).ans;
 }
 
 int main() {
@@ -45,26 +42,19 @@ int main() {
 	cin.tie(0);
 	cin >> s;
 	n = s.size();
-	op.resize(n);
-	t.assign((n << 1), vector<node>(2));
+	t.resize(n << 1);
 	for(int i = 0; i < n; i++) {
-		if(i) op[i] += op[i - 1];
 		if(s[i] == '(') {
-			t[i + n][0] = t[i + n][0] + i;
-			op[i]++;
+			t[i + n].l = 1;
 		} else {
-			t[i + n][1] = t[i + n][1] + i;
+			t[i + n].r = 1;
 		}
-	} build(0), build(1);
-	for(auto &u : op) cout << u << " \n"[&u == &op.back()];
-	return 0;
+	} build();
 	cin >> m;
 	while(m--) {
 		int x, y;
 		cin >> x >> y;
-		auto open = query(x, y, 0);
-		auto close = query(x, y, 1);
-
+		cout << (query(--x, --y) << 1) << '\n';
 	}
 	return 0;
 }
