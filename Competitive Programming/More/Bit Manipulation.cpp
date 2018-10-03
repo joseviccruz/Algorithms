a6757cd054988e1e545fa3c6c9dce5aff0da409a
José Victor Cruz <jvsc@cin.ufpe.br>
2018-10-02 21:31:02 -0300

    Update Bit Manipulation.cpp

--- Competitive Programming/More/Bit Manipulation.cpp
+++ Competitive Programming/More/Bit Manipulation.cpp
@@ -13,7 +13,7 @@ inline void flip_bit(T &n, const int b) { n ^= (T(1) << b); }
 template<typename T>
 inline int count_bits(const T &n) {
 	int ans = 0;
-	for(register T x = n; x; x >>= 1) {
+	for(T x = n; x; x >>= 1) {
 		ans += (x & 1);
 	}
 	return ans;
