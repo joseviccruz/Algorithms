c1f69cf9b78604b26549acf0914beba58639b2c1
José Victor Cruz <jvsc@cin.ufpe.br>
2019-02-13 01:06:00 -0300

    Update and rename union_find.cpp to dsu_t.cpp

---
+++ Competitive Programming/Data Structures/dsu_t.cpp
@@ -0,0 +1,23 @@
+struct dsu_t {
+  int n;
+  vector<int> cnj;
+
+public:
+  dsu_t(int _n) : n(_n), cnj(n) {
+    iota(cnj.begin(), cnj.end(), 0);
+  }
+  
+  inline int root(int x) {
+    return (x == cnj[x] ? x : (cnj[x] = root(cnj[x])));
+  }
+  
+  inline bool join(int x, int y) {
+    x = root(x);
+    y = root(y);
+    if(x != y) {
+      cnj[x] = y;
+      return true;
+    }
+    return false;
+  }
+};
