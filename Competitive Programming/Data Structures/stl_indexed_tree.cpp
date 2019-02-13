f593f9fbf24e4df79fca9e53d07ec065e836815e
José Victor Cruz <jvsc@cin.ufpe.br>
2019-02-13 00:42:08 -0300

    Update stl_indexed_tree.cpp

--- Competitive Programming/Data Structures/stl_indexed_tree.cpp
+++ Competitive Programming/Data Structures/stl_indexed_tree.cpp
@@ -1,7 +1,5 @@
-#include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 
-using namespace std;
 using namespace __gnu_pbds;
 
 template <typename Key, typename T = null_type, typename Compare = less<Key>>
@@ -14,10 +12,3 @@ using indexed_tree = tree<Key, T, Compare, rb_tree_tag, tree_order_statistics_no
  * .order_of_key(const Key x)      - return the number of elements less than 'x'
  * .find_by_order(const size_t n) - return the 'n-th' (0-indexed) smallest number stored in this tree
 **/
-
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	
-	return 0;
-}
