1b1129744002b5cc9b79946cc379f81ec2304027
José Victor Cruz <jvsc@cin.ufpe.br>
2019-02-13 00:40:25 -0300

    Update segtree_t.cpp

--- Competitive Programming/Data Structures/segtree_t.cpp
+++ Competitive Programming/Data Structures/segtree_t.cpp
@@ -1,69 +1,59 @@
 struct node_t {
-  int x;
+  // atributes, don't forget to set default value
   
-  node_t(int _x = 0) {
-    x = _x;
+  node_t(...) {
+    ...
   }
   
   node_t(const node_t &lhs, const node_t &rhs) {
-    x = max(lhs.x, rhs.x);
+    // merge
   }
 };
 
 template <class n_t>
 class segtree_t {
   int n;
-  vector<n_t> st;
+  vector<n_t> tree;
   
   void build() {
-    fill(st.begin() + n, st.end(), n_t());
-    for (int x = n - 1; x; x--)
-      st[x] = n_t(st[x + x], st[x + x + 1]);
-  }
-  
-  template <class T>
-  void build(const vector<T> &base) {
-    for (int x = 0; x < n; x++)
-      st[x + n] = n_t(base[x]);
-    for (int x = n - 1; x; x--)
-      st[x] = n_t(st[x + x], st[x + x + 1]);
+    for (int x = n - 1; x > 0; x--)
+      tree[x] = n_t(tree[x + x], tree[x + x + 1]);
   }
 
 public:
-  template <class T>
-  void modify(int x, T y) {
-    assert(x >= 0 && x < n);
-    for (st[x += n] = n_t(y), x >>= 1; x; x >>= 1)
-      st[x] = n_t(st[x + x], st[x + x + 1]);
-  }
-  
-  n_t query(int l, int r) {
-    assert(l >= 0 && l < n);
-    assert(r >= 0 && r < n);
-    n_t lhs, rhs;
-    for (l += n, r += n + 1; l < r; l >>= 1, r >>= 1) {
-      if (l & 1)
-        lhs = n_t(lhs, st[l++]);
-      if (r & 1)
-        rhs = n_t(st[--r], rhs);
-    }
-    return n_t(lhs, rhs);
-  }
-  
-  n_t query(int x) {
-    return st[x + n];
-  }
-  
-  segtree_t(int _n) {
-    n = _n;
-    st.resize(n + n);
+  segtree_t(int _n) : n(_n) {
+    tree.assign(n + n, n_t());
     build();
   }
   
   template <typename T>
-  segtree_t(const vector<T> &base) {
-    n = base.size();
-    st.resize(n + n);
-    build(base);
+  segtree_t(const vector<T> &base) : n(base.size()) {
+    tree.resize(n);
+    copy(base.begin(), base.end(), back_inserter(tree));
+    build();
+  }
+
+  template <class T>
+  void modify(int x, T y) {
+    assert(x >= 0 && x < n);
+    for (tree[x += n] = n_t(y), x /= 2; x > 0; x /= 2)
+      tree[x] = n_t(tree[x + x], tree[x + x + 1]);
+  }
+  
+  n_t get(int l, int r) {
+    assert(l >= 0 && l < n);
+    assert(r >= 0 && r < n);
+    n_t lhs{}, rhs{};
+    for (l += n, r += n + 1; l < r; l /= 2, r /= 2) {
+      if (l & 1)
+        lhs = n_t(lhs, tree[l++]);
+      if (r & 1)
+        rhs = n_t(tree[--r], rhs);
+    }
+    return n_t(lhs, rhs);
+  }
+  
+  n_t get(int x) {
+    return tree[x + n];
   }
 };
