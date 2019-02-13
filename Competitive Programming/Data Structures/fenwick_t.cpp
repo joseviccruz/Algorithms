f92033a3b5b663a447d9b6a469876a214650d520
José Victor Cruz <jvsc@cin.ufpe.br>
2019-02-13 00:20:36 -0300

    Update and rename fenwick.cpp to fenwick_t.cpp

---
+++ Competitive Programming/Data Structures/fenwick_t.cpp
@@ -0,0 +1,36 @@
+struct node_t {
+  // atributes, don't forget to set default value
+  
+  inline void operator += (node_t &other) {
+    
+  }
+};
+
+template <typename T>
+class fenwick_t {
+  int n;
+  vector<T> tree;
+
+public:
+  fenwick_t(int _n) : n(_n) {
+    tree.resize(n);
+  }
+  
+  void modify(int x, T v) {
+    assert(x >= 0 && x < n);
+    while (x < n) {
+      tree[x] += v;
+      x |= (x + 1);
+    }
+  }
+  
+  T get(int x) {
+    assert(x >= 0 && x < n);
+    T v{};
+    while (x >= 0) {
+      v += tree[x];
+      x = (x & (x + 1)) - 1;
+    }
+    return v;
+  }
+};
