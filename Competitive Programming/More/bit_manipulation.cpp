c099946ce31a977117b807b2ff4d68c584f77316
José Victor Cruz <jvsc@cin.ufpe.br>
2019-02-13 01:24:39 -0300

    Update and rename Bit Manipulation.cpp to bit_manipulation.cpp

--- Competitive Programming/More/bit_manipulation.cpp
+++ Competitive Programming/More/bit_manipulation.cpp
@@ -1,26 +1,26 @@
-template<typename T>
+/*
+  Things to remember:
+    - std::bitset<size_t> just have '==' and '!=' defined;
+    - ...
+*/
+
+template <class T>
 inline bool check_bit(const T &n, const int b) { return n & (T(1) << b); }
 
-template<typename T>
+template <class T>
 inline void set_bit(T &n, const int b) { n |= (T(1) << b); }
 
-template<typename T>
+template <class T>
 inline void remove_bit(T &n, const int b) { n &= ~(T(1) << b); }
 
-template<typename T>
+template <class T>
 inline void flip_bit(T &n, const int b) { n ^= (T(1) << b); }
 
-template<typename T>
+template <class T>
 inline int count_bits(const T &n) {
-	int ans = 0;
-	for(T x = n; x; x >>= T(1)) {
-		ans += (x & T(1));
-	}
-	return ans;
+  int ans = 0;
+  for (T x = n; x >= 0; x >>= T(1)) {
+    ans += (x & T(1));
+  }
+  return ans;
 }
-
-/*
-	Things to remember:
-	- std::bitset<size_t> just have '==' and '!=' defined;
-	- ...
-*/
