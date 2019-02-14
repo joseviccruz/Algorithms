730338b00f56e78a0a50f1f8d8b3050f7e10d7e1
José Victor Cruz <jvsc@cin.ufpe.br>
2019-02-14 09:49:46 -0300

    Update z_function.cpp

--- Competitive Programming/Strings/z_function.cpp
+++ Competitive Programming/Strings/z_function.cpp
@@ -3,9 +3,8 @@ vector<int> z_function(int n, const T &s) {
   vector<int> z(n);
   for (int i = 1, l = 0, r = 0; i < n; i++) {
     z[i] = (i > r) ? 0 : min(r - i + 1, z[i - l]);
-    while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+    while (i + z[i] < n && s[z[i]] == s[i + z[i]])
       z[i]++;
-    }
     if (i + z[i] - 1 > r) {
       l = i;
       r = i + z[i] - 1;
