09b27ba0c43f139231726017aa8905cccb2fb8e4
José Victor Cruz <jvsc@cin.ufpe.br>
2019-02-16 16:55:03 -0300

    Update kmp.cpp

--- Competitive Programming/Strings/kmp.cpp
+++ Competitive Programming/Strings/kmp.cpp
@@ -1,37 +1,35 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-string S, U;
-
-vector<int> generate(const string& s) {
-	vector<int> v(s.length() + 1, 0);
-	for(int i = 1; i < s.length(); i++) {
-		int idx = v[i];
-		while(idx && s[i] != s[idx]) idx = v[idx];
-		idx += (s[i] == s[idx]);
-		v[i + 1] = idx;
-	}
-	return v;
+template <class T>
+vector<int> kmp_table(int n, const T &s) {
+  vector<int> p(n);
+  for (int i = 1, j = 0; i < n; i++) {
+    while (j > 0 && s[i] != s[j])
+      j = p[j - 1];
+    j += s[i] == s[j];
+    p[i] = j;
+  }
+  return p;
 }
 
-int query(const string& a, const string& b) {
-	auto v = generate(b);
-	int idx = 0, qnt = 0;
-	for(int i = 0; i < a.length(); i++) {
-		while(idx && a[i] != b[idx]) idx = v[idx];
-		idx += (a[i] == b[idx]);
-		if(idx == b.length()) {
-			idx = v[idx];
-			++qnt;
-		}
-	}
-	return qnt;
+template <class T>
+vector<int> kmp_table(const T &s) {
+  return kmp_table(s.size(), s);
 }
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	
-	return 0;
+template <class T>
+vector<int> kmp_search(int n, const T &s, int m, const T &u, const vector<int> &p) {
+  assert(m >= 1 && m == p.size());
+  vector<int> ans;
+  for (int i = 0, j = 0; i < n; i++) {
+    while (j > 0 && (j == m || s[i] != u[j]))
+      j = p[j - 1];
+    j += s[i] == u[j];
+    if (j == m)
+      ans.push_back(i - j + 1);
+  }
+  return ans;
+}
+
+template <class T>
+vector<int> kmp_search(const T &s, const T &u) {
+  return kmp_search(s.size(), s, u.size(), u, kmp_table(u));
 }
