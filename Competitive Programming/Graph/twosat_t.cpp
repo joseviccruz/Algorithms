e15ef44faedc48473a1dd147c269faf650902b45
José Victor Cruz <jvsc@cin.ufpe.br>
2019-02-20 18:43:19 -0300

    Update twosat_t.cpp

--- Competitive Programming/Graph/twosat_t.cpp
+++ Competitive Programming/Graph/twosat_t.cpp
@@ -10,32 +10,35 @@
 class twosat_t {
   int n;
   int g_n;
-  vector<bool> mark;
-  vector<int> order, cmp;
-  vector<vector<int>> adj, radj;
-
-  void dfs_st(int v) {
-    mark[v] = true;
-    for (int u : adj[v])
-      if (!mark[u])
-        dfs_st(u);
-    order.push_back(v);
-  }
-
-  void dfs_nd(int v) {
-    for (int u : radj[v])
-      if (cmp[u] == -1) {
-        cmp[u] = cmp[v];
-        dfs_nd(u);
-      }
-  }
+  vector<int> cmp;
+  vector<vector<int>> adj;
 
   void scc() {
+    vector<vector<int>> radj(g_n);
+    for (int v = 0; v < g_n; v++)
+      for (int u : adj[v])
+        radj[u].push_back(v);
+    vector<int> order;
+    vector<bool> mark(g_n);
+    function<void(int)> dfs_st = [&](int v) {
+      mark[v] = true;
+      for (int u : adj[v])
+        if (!mark[u])
+          dfs_st(u);
+      order.push_back(v);
+    };
     for (int i = 0; i < g_n; i++)
       if (!mark[i])
         dfs_st(i);
     int cnt = 0;
     cmp.assign(g_n, -1);
+    function<void(int)> dfs_nd = [&](int v) {
+      for (int u : radj[v])
+        if (cmp[u] == -1) {
+          cmp[u] = cmp[v];
+          dfs_nd(u);
+        }
+    };
     for (int i = g_n - 1; i >= 0; i--) {
       int v = order[i];
       if (cmp[v] != -1)
@@ -64,12 +67,6 @@ public:
   }
 
   vector<int> solve() {
-    order.clear();
-    mark.assign(g_n, false);
-    radj.assign(g_n, vector<int>());
-    for (int v = 0; v < g_n; v++)
-      for (int u : adj[v])
-        radj[u].push_back(v);
     scc();
     vector<int> ans(n);
     for (int i = 0; i < n; i++) {
