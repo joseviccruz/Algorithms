c5a7c1df34afe8d866cf537bdfd3d5b907da1e77
José Victor Cruz <jvsc@cin.ufpe.br>
2019-02-24 21:57:00 -0300

    Create template.cpp

---
+++ Competitive Programming/Miscellaneous/template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+mt19937_64 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  
+  return 0;
+}
