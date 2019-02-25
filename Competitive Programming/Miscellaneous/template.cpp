6fac7e0426b4d16c3502b46568cc2b4e0d6c0cd9
José Victor Cruz <jvsc@cin.ufpe.br>
2019-02-24 21:57:11 -0300

    Update template.cpp

--- Competitive Programming/Miscellaneous/template.cpp
+++ Competitive Programming/Miscellaneous/template.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-mt19937_64 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
 
 int main() {
   ios::sync_with_stdio(false);
