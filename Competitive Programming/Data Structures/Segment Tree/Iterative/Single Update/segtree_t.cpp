8c6df3468c2f692683ac8429b3dcc987beea2080
José Victor Cruz <jvsc@cin.ufpe.br>
2019-02-26 22:06:52 -0300

    Update segtree_t.cpp

--- Competitive Programming/Data Structures/Segment Tree/Iterative/Single Update/segtree_t.cpp
+++ Competitive Programming/Data Structures/Segment Tree/Iterative/Single Update/segtree_t.cpp
@@ -19,25 +19,26 @@ class segtree_t {
 
 public:
   segtree_t(int _n) : n(_n), tree(_n + _n) {
+    assert(n > 0);
     build();
   }
   
   template <class T>
   segtree_t(const vector<T> &base) : n(base.size()), tree(base.size()) {
+    assert(n > 0);
     copy(base.begin(), base.end(), back_inserter(tree));
     build();
   }
 
   template <class... Args>
   void modify(int x, Args&&... args) {
-    assert(x >= 0 && x < n);
+    assert(x >= 0 && x <= n - 1);
     for (tree[x += n] = n_t(args...), x >>= 1; x > 0; x >>= 1)
       tree[x] = n_t(tree[x + x], tree[x + x + 1]);
   }
   
   n_t get(int l, int r) {
-    assert(l >= 0 && l < n);
-    assert(r >= 0 && r < n);
+    assert(0 <= l && l <= r && r <= n - 1);
     n_t lhs{}, rhs{};
     for (l += n, r += n + 1; l < r; l >>= 1, r >>= 1) {
       if (l & 1)
@@ -49,6 +50,7 @@ public:
   }
   
   n_t get(int x) {
+    assert(x >= 0 && x <= n - 1);
     return tree[x + n];
   }
 };
