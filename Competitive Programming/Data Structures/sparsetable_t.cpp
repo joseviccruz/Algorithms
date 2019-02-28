e6a1945dff1070f45a2b2021b97b6274dc90730c
José Victor Cruz <jvsc@cin.ufpe.br>
2019-02-28 10:51:34 -0300

    Update sparsetable_t.cpp

--- Competitive Programming/Data Structures/sparsetable_t.cpp
+++ Competitive Programming/Data Structures/sparsetable_t.cpp
@@ -3,12 +3,16 @@ class sparsetable_t {
 public:
   int n;
   const F func;
+  vector<int> lg;
   vector<vector<T>> table;
   
   sparsetable_t(const vector<T> &base, const F &func) : func(func) {
     n = base.size();
     assert(n > 0);
-    int max_log = __lg(n) + 1;
+    lg.resize(n + 1);
+    for (int i = 2; i <= n; i++)
+      lg[i] = lg[i >> 1] + 1;
+    int max_log = lg[n] + 1;
     table.push_back(base);
     table.resize(max_log);
     for (int j = 1; j < max_log; j++) {
@@ -20,7 +24,7 @@ public:
   
   T get(int x, int y) {
     assert(0 <= x && x <= y && y <= n - 1);
-    int pos = __lg(y - x + 1);
+    int pos = lg[y - x + 1];
     return func(table[pos][x], table[pos][y - (1 << pos) + 1]);
   }
 };
