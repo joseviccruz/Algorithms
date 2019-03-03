4bd5ee62814e821581b8c4a4affc9c9fe27d4fd4
José Victor Cruz <jvsc@cin.ufpe.br>
2019-03-03 11:23:06 -0300

    Update bit_manipulation.cpp

--- Competitive Programming/Miscellaneous/bit_manipulation.cpp
+++ Competitive Programming/Miscellaneous/bit_manipulation.cpp
@@ -1,9 +1,3 @@
-/*
-  Things to remember:
-    - std::bitset<size_t> just have '==' and '!=' defined;
-    - ...
-*/
-
 template <class T>
 inline bool check_bit(const T &n, const int b) { return n & (T(1) << b); }
 
@@ -15,12 +9,3 @@ inline void remove_bit(T &n, const int b) { n &= ~(T(1) << b); }
 
 template <class T>
 inline void flip_bit(T &n, const int b) { n ^= (T(1) << b); }
-
-template <class T>
-inline int count_bits(const T &n) {
-  int ans = 0;
-  for (T x = n; x >= 0; x >>= T(1)) {
-    ans += (x & T(1));
-  }
-  return ans;
-}
