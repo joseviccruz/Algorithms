acd542778ed669762aec8c38b9d3e96eceefe09c
José Victor Cruz <jvsc@cin.ufpe.br>
2019-03-31 17:54:44 -0300

    Update matrix_t.cpp

--- Competitive Programming/Number Theory/matrix_t.cpp
+++ Competitive Programming/Number Theory/matrix_t.cpp
@@ -1,16 +1,45 @@
-template <class T>
-using matrix_t = vector<vector<T>>;
+template <int N, int M, class T = int>
+class matrix_t {
+public:
+  static constexpr int n = N;
+  static constexpr int m = M;
+  array<array<T, m>, n> mat;
 
-template <class T>
-matrix_t<T> operator * (const matrix_t<T> &lhs, const matrix_t<T> &rhs) {
-  assert(lhs.front().size() == rhs.size());
-  int rows = lhs.size();
-  int cowls = rhs.front().size();
-  int common = rhs.size();
-  matrix_t<T> result(rows, vector<T>(cowls));
-  for(int i = 0; i < rows; i++)
-    for(int j = 0; j < cowls; j++)
-      for(int k = 0; k < common; k++)
-        result[i][j] += lhs[i][k] * rhs[k][j];
-  return result;
-}
+  inline array<T, m> &operator [] (const size_t &x) {
+    return mat[x];
+  }
+
+  inline matrix_t operator + (const matrix_t &other) {
+    matrix_t ret{};
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++)
+        ret.mat[i][j] = add(mat[i][j], other.mat[i][j]);
+    return ret;
+  }
+
+  inline matrix_t operator - (const matrix_t &other) {
+    matrix_t ret{};
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++)
+        ret.mat[i][j] = sub(mat[i][j], other.mat[i][j]);
+    return ret;
+  }
+
+  inline matrix_t operator * (const T &t) {
+    matrix_t ret{};
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++)
+        ret.mat[i][j] = fmul(mat[i][j], t);
+    return ret;
+  }
+
+  template <int o> // [n][m] * [m][o]
+  inline matrix_t<n, o> operator * (const matrix_t<m, o> &other) {
+    matrix_t<n, o> ret{};
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < o; j++)
+        for (int k = 0; k < m; k++)
+          ret.mat[i][j] = add(ret.mat[i][j], fmul(mat[i][k], other.mat[k][j]));
+    return ret;
+  }
+};
