0564375f2e54e2c448db937b40778540752a98cb
José Victor Cruz <jvsc@cin.ufpe.br>
2019-04-08 02:40:55 -0300

    Update gcd.cpp

--- Competitive Programming/Number Theory/gcd.cpp
+++ Competitive Programming/Number Theory/gcd.cpp
@@ -68,3 +68,31 @@ bool diophantine(T a, T b, T c, T &x, T &y, T &g) {
     y = -y;
   return true;
 }
+
+template <class T>
+bool crt(T k1, T m1, T k2, T m2, T &k, T &m) {
+  if (k1 %= m1 < 0) k1 += m1;
+  if (k2 %= m2 < 0) k2 += m2;
+  T x, y, g;
+  if (!diophantine(m1, -m2, k2 - k1, x, y, g))
+    return false;
+  T dx = m2 / g;
+  T delta = x / dx - (x % dx < 0);
+  k = m1 * (x - dx * delta) + k1;
+  m = m1 / g * m2;
+  assert(0 <= k && k < m);
+  return true;
+}
+
+template <class T>
+bool crt(const vector<pair<T, T>> &v, T &k, T &m) {
+  assert(!v.empty());
+  T a, p;
+  tie(k, m) = v[0];
+  for (int i = 1; i < (int) v.size(); i++) {
+    tie(a, p) = v[i];
+    if (!crt(k, m, a, p, k, m))
+      return false;
+  }
+  return true;
+}
