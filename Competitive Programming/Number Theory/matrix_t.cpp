f350c7492b08fe69b6a67426038df3e543234f39
José Victor Cruz <jvsc@cin.ufpe.br>
2019-04-16 22:14:07 -0300

    Update matrix_t.cpp

--- Competitive Programming/Number Theory/matrix_t.cpp
+++ Competitive Programming/Number Theory/matrix_t.cpp
@@ -1,16 +1,8 @@
-template <int N, int M, class T = int>
+template <int n, int m, class T = int>
 class matrix_t {
-  array<array<T, M>, N> mat;
+  array<array<T, m>, n> mat;
 
 public:
-  static constexpr int n = N;
-  static constexpr int m = M;
-
-  inline array<T, m> &operator [] (const size_t &x) {
-    assert(0 <= x && x <= m);
-    return mat[x];
-  }
-
   matrix_t() : mat({}) {
   }
 
@@ -25,28 +17,9 @@ public:
     }
   }
 
-  inline matrix_t operator + (const matrix_t &other) {
-    matrix_t ret;
-    for (int i = 0; i < n; i++)
-      for (int j = 0; j < m; j++)
-        ret.mat[i][j] = add(mat[i][j], other.mat[i][j]);
-    return ret;
-  }
-
-  inline matrix_t operator - (const matrix_t &other) {
-    matrix_t ret;
-    for (int i = 0; i < n; i++)
-      for (int j = 0; j < m; j++)
-        ret.mat[i][j] = sub(mat[i][j], other.mat[i][j]);
-    return ret;
-  }
-
-  inline matrix_t operator * (const T &t) {
-    matrix_t ret;
-    for (int i = 0; i < n; i++)
-      for (int j = 0; j < m; j++)
-        ret.mat[i][j] = fmul(mat[i][j], t);
-    return ret;
+  inline array<T, m> &operator [] (const size_t &x) {
+    assert(0 <= x && x <= m);
+    return mat[x];
   }
 
   template <int o>
