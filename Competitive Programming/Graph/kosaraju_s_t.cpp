b451320329d666bc78f8cb427920ac91fafac14e
José Victor Cruz <jvsc@cin.ufpe.br>
2019-06-20 01:37:18 -0300

    Update kosaraju_s_t.cpp

--- Competitive Programming/Graph/kosaraju_s_t.cpp
+++ Competitive Programming/Graph/kosaraju_s_t.cpp
@@ -1,29 +1,34 @@
+template <class T>
 class kosaraju_s_t {
 public:
+  struct edge_t {
+    int to;
+    T w;
+  };
   int n;
-  vector<vector<int>> adj;
+  vector<vector<edge_t>> adj;
 
   kosaraju_s_t(int n) : n(n) {
     assert(n > 0);
     adj.resize(n);
   }
 
-  void add(int x, int y) {
-    adj[x].push_back(y);
+  void add(int x, int y, T w = 1) {
+    adj[x].push_back({y, w});
   }
   
   vector<int> scc() {
-    vector<vector<int>> radj(n);
+    vector<vector<edge_t>> radj(n);
     for (int v = 0; v < n; v++)
-      for (int u : adj[v])
-        radj[u].push_back(v);
+      for (edge_t e : adj[v])
+        radj[e.to].push_back({v, e.w});
     vector<int> order;
     vector<bool> mark(n);
     function<void(int)> dfs_st = [&](int v) {
       mark[v] = true;
-      for (int u : adj[v])
-        if (!mark[u])
-          dfs_st(u);
+      for (edge_t e : adj[v])
+        if (!mark[e.to])
+          dfs_st(e.to);
       order.push_back(v);
     };
     for (int i = 0; i < n; i++)
@@ -32,10 +37,10 @@ public:
     vector<int> component(n);
     component.assign(n, -1);
     function<void(int)> dfs_nd = [&](int v) {
-      for (int u : radj[v])
-        if (component[u] == -1) {
-          component[u] = component[v];
-          dfs_nd(u);
+      for (edge_t e : radj[v])
+        if (component[e.to] == -1) {
+          component[e.to] = component[v];
+          dfs_nd(e.to);
         }
     };
     int cnt = 0;
