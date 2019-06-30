cdc9efc6430bb847e3e7cb41f6b0f13a6f26124a
José Victor Cruz <jvsc@cin.ufpe.br>
2019-06-30 14:46:11 -0300

    Update polar_cmp.cpp

--- Competitive Programming/Geometry/polar_cmp.cpp
+++ Competitive Programming/Geometry/polar_cmp.cpp
@@ -1,10 +1,9 @@
-template <class T>
 struct polar_cmp {
-  const pt_t<T> pivot;
+  const pt_t pivot;
   template <class... Args>
   polar_cmp(const Args&... args) : pivot(args...) {
   }
-  inline bool operator () (const pt_t<T> &p, const pt_t<T> &q) {
+  inline bool operator () (const pt_t &p, const pt_t &q) {
     return cross(p - pivot, q - pivot) > 0.0;
   }
 };
