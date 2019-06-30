82998d453ba92840f02136625db1355e6ed14d3b
José Victor Cruz <jvsc@cin.ufpe.br>
2019-06-30 16:31:58 -0300

    Update polar_cmp.cpp

--- Competitive Programming/Geometry/polar_cmp.cpp
+++ Competitive Programming/Geometry/polar_cmp.cpp
@@ -1,9 +1,24 @@
+using namespace rel_ops;
+
 struct polar_cmp {
-  const pt_t pivot;
+  const pt_t center;
   template <class... Args>
-  polar_cmp(const Args&... args) : pivot(args...) {
+  polar_cmp(const Args&... args) : center(args...) {
   }
   inline bool operator () (const pt_t &p, const pt_t &q) {
-    return cross(p - pivot, q - pivot) > 0.0;
+    return cross(p - center, q - center) < 0;
   }
 };
+
+template <class RandomIt, class... Args>
+typename enable_if<is_same<typename iterator_traits<RandomIt>::value_type, pt_t>::value>::type
+sort_polar(RandomIt first, RandomIt last, const pt_t& center) {
+  first = partition(first, last, [&center](const pt_t& point) {
+    return point == center;
+  });
+  auto pivot = partition(first, last, [&center](const pt_t& point) {
+    return point > center;
+  });
+  sort(first, pivot, polar_cmp(center));
+  sort(pivot, last, polar_cmp(center));
+}
