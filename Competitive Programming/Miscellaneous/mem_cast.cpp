90f215bb96d3ff8cb2774980987b596649d95e0d
José Victor Cruz <jvsc@cin.ufpe.br>
2019-12-06 13:05:55 -0300

    Update mem_cast.cpp

--- Competitive Programming/Miscellaneous/mem_cast.cpp
+++ Competitive Programming/Miscellaneous/mem_cast.cpp
@@ -1,5 +1,5 @@
 template <class T, class U>
-inline T mem_cast(const U &y) {
+inline const T &mem_cast(const U &y) {
   static T x;
   static_assert(sizeof x == sizeof y);
   memcpy(&x, &y, sizeof y);
