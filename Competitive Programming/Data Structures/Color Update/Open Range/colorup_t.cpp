3410884a614d7f1cab52e057e8bbd60353023f64
José Victor Cruz <jvsc@cin.ufpe.br>
2019-12-09 01:36:38 -0300

    Create colorup_t.cpp

---
+++ Competitive Programming/Data Structures/Color Update/Open Range/colorup_t.cpp
@@ -0,0 +1,47 @@
+template <class T, class Int = int>
+class colorup_t {
+public:
+  struct range_t {
+    Int l, r;
+    T v;
+    
+    range_t(Int l = 0, Int r = 0, T v = {}) : l(l), r(r), v(v) {
+    }
+    
+    inline bool operator < (const range_t &other) const {
+      return l < other.l;
+    }
+  };
+  set<range_t> ranges;
+
+  vector<range_t> modify(Int l, Int r, T v) {
+    vector<range_t> ret;
+    if (l >= r)
+      return ret;
+    auto it = ranges.lower_bound(l);
+    if (it != ranges.begin()) {
+      it--;
+      if (it->r > l) {
+        auto er = *it;
+        ranges.erase(it);
+        ranges.emplace(er.l, l, er.v);
+        ranges.emplace(l, er.r, er.v);
+      }
+    }
+    it = ranges.lower_bound(r);
+    if (it != ranges.begin()) {
+      it--;
+      if (it->r > r) {
+        auto er = *it;
+        ranges.erase(it);
+        ranges.emplace(er.l, r, er.v);
+        ranges.emplace(r, er.r, er.v);
+      }
+    }
+    for (it = ranges.lower_bound(l); it != ranges.end() && it->l < r; it++)
+      ret.push_back(*it);
+    ranges.erase(ranges.lower_bound(l), ranges.lower_bound(r));
+    ranges.emplace(l, r, v);
+    return ret;
+  }
+};
