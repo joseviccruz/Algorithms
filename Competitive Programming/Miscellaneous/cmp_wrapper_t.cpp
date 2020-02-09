6fbc36fbf28d2dddc6afe65b501bbd4188526e32
José Victor Cruz <jvsc@cin.ufpe.br>
2020-02-09 17:01:07 -0300

    Create cmp_wrapper_t.cpp

---
+++ Competitive Programming/Miscellaneous/cmp_wrapper_t.cpp
@@ -0,0 +1,28 @@
+template <class T>
+class wrapper {
+  T value;
+
+ public:
+  static constexpr T eps = 1e-6;
+ 
+  constexpr wrapper() : value() { }
+  template <class U> constexpr wrapper(const U &_value) : value(_value) { }
+  
+  template <class U> bool operator ==(const U &other) const { return abs(value - wrapper(other).value) <= eps; }
+  template <class U> bool operator !=(const U &other) const { return !(*this == other); }
+
+  template <class U> bool operator  <(const U &other) const { return !(*this == other) && (value < wrapper(other).value); }
+  template <class U> bool operator  >(const U &other) const { wrapper(other) < *this; }
+  template <class U> bool operator <=(const U &other) const { !(*this > other) || (*this == other); }
+  template <class U> bool operator >=(const U &other) const { !(*this < other) || (*this == other); }
+
+  template <class U> friend bool operator ==(const U &lhs, const wrapper &rhs) { return wrapper(lhs).operator ==(rhs); }
+  template <class U> friend bool operator !=(const U &lhs, const wrapper &rhs) { return wrapper(lhs).operator !=(rhs); }
+
+  template <class U> friend bool operator  <(const U &lhs, const wrapper &rhs) { return wrapper(lhs).operator  <(rhs); }
+  template <class U> friend bool operator  >(const U &lhs, const wrapper &rhs) { return wrapper(lhs).operator  >(rhs); }
+  template <class U> friend bool operator <=(const U &lhs, const wrapper &rhs) { return wrapper(lhs).operator <=(rhs); }
+  template <class U> friend bool operator >=(const U &lhs, const wrapper &rhs) { return wrapper(lhs).operator >=(rhs); }
+
+  operator T &() { return value; }
+};
